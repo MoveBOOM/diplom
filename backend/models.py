67742fbf4d2e89546c1ec880c6b977f8d739a75a
erinsergey37 <s.erin@big3.ru>
2024-07-28 16:33:37 +0300

    Add migrations

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,17 @@
+import django
 from django.db import models
 
 
 class Shop(models.Model):
     name = models.CharField(max_length=128)
     url = models.URLField(verbose_name='URL')
+
+
+class Category(models.Model):
+    shops = models.ManyToManyField(Shop, related_name='categories')
+    name = models.CharField(max_length=128)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=128)
